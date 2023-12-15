6548259bb4d15be607374e81cb897b6a5c201a9b
ed.xyz <eddietang2314@gmail.com>
2023-12-15 15:19:09 -0500

    Added model tests for Location, Box, Item, ItemPortion, and Unit.

---
+++ djangobox/box/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from models import Location, Box, Item, ItemPortion, Unit
+
+
+class ModelsTestCase(TestCase):
+    def setUp(self):
+        # Create test data for models
+        self.location = Location.objects.create(name="Test Location")
+        self.box = Box.objects.create(name="Test Box", location=self.location)
+        self.unit = Unit.objects.create(unit_name="Test Unit")
+
+    def test_location_str(self):
+        self.assertEqual(str(self.location), "Test Location")
+
+    def test_box_str(self):
+        self.assertEqual(str(self.box), "Test Box")
+
+    def test_item_str(self):
+        item = Item.objects.create(name="Test Item", unit=self.unit)
+        self.assertEqual(str(item), "Test Item")
+
+    def test_item_portion_str(self):
+        item_portion = ItemPortion.objects.create(
+            item=Item.objects.create(name="Test Item"), box=self.box
+        )
+        self.assertEqual(str(item_portion), "Test Item<>Test Box")
+
+    def test_unit_str(self):
+        self.assertEqual(str(self.unit), "Test Unit")
